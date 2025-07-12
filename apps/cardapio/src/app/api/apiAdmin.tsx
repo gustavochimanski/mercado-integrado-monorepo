df40a17870d8240167c55a00cdd0b3afeb6c60b5
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-07-12 09:59:51 -0300

    TOKEN IFRAME ARMAZENADO EM VAR

--- apps/cardapio/src/app/api/apiAdmin.tsx
+++ apps/cardapio/src/app/api/apiAdmin.tsx
@@ -1,21 +1,19 @@
+// src/lib/api/apiMensura.ts
+"use client";
 
-import { setToken } from "@packs/auth/src/tokenStore";
-import { useEffect } from "react";
+import { getToken } from "@packs/auth/src/tokenStore";
+import axios from "axios";
 
-export function useReceiveTokenFromParent() {
-  useEffect(() => {
-    window.parent.postMessage({ type: "ready_for_token" }, "*");
+const apiAdmin = axios.create({
+  baseURL: "https://gerente.mensuraapi.com.br",
+});
 
-    const listener = (event: MessageEvent) => {
-      const { type, token } = event.data || {};
-      if (type === "auth_token" && token) {
-        console.log("🔐 Token recebido:", token);
-        setToken(token); 
-      }
-    };
+apiAdmin.interceptors.request.use((config) => {
+  const token = getToken(); // ✅ vem da memória
+  if (token && typeof token === "string") {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-    window.addEventListener("message", listener);
-    return () => window.removeEventListener("message", listener);
-  }, []);
-}
-
+export default apiAdmin;"use client";
