c4d5197b7ea905315955281b78fbcf3f4517408d
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-07-14 11:33:38 -0300

    BUILD OK

--- apps/supervisor/src/app/(private)/cardapio/page.tsx
+++ apps/supervisor/src/app/(private)/cardapio/page.tsx
@@ -100,7 +100,7 @@ export default function PageAdminCardapio() {
               </div>
               <iframe
                 ref={iframeRef}
-                src={linkDev}
+                src={linkProd}
                 className="w-full flex-1"
               />
             </div>
