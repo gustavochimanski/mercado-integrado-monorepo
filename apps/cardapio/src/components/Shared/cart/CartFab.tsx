69f6d1c49eb8d1cdf82ebf8d3d250cbd93d7f67a
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-09-06 09:29:04 -0300

    .

--- apps/cardapio/src/components/Shared/cart/CartFab.tsx
+++ apps/cardapio/src/components/Shared/cart/CartFab.tsx
@@ -2,32 +2,38 @@
 import { ShoppingCart } from "lucide-react";
 import { useCart } from "@cardapio/stores/cart/useCart";
 import { Button } from "../ui/button";
+import { usePathname } from "next/navigation";
 
 interface Props {
   onOpen: () => void;
 }
 
 export function CartFab({ onOpen }: Props) {
-  const total = useCart((s) => s.items.reduce((acc, i) => acc + i.quantity, 0));
+  const total = useCart((s) =>
+    s.items.reduce((acc, i) => acc + i.quantity, 0)
+  );
   const totalPrice = useCart((s) =>
     s.items.reduce((acc, i) => acc + i.quantity * i.preco, 0)
   );
 
-  if (total === 0) return null;
+  const pathname = usePathname();
+
+  // Oculta se não houver itens ou se estiver na rota /finalizar-pedido
+  if (total === 0 || pathname === "/finalizar-pedido") return null;
+  if (total === 0 || pathname === "/menu") return null;
 
   return (
     <div className="fixed bottom-20 left-0 right-0 z-50 flex justify-center px-4">
       <Button
         onClick={onOpen}
-        className="relative w-full max-w-md flex items-center justify-between gap-3 rounded-full px-5 py-5   shadow-xl bg-primary text-background"
+        className="relative w-full max-w-md flex items-center justify-between gap-3 rounded-full px-5 py-5 shadow-xl bg-primary text-background"
       >
-        {/* Ícone + badge (mantendo estilo antigo) */}
+        {/* Ícone */}
         <div className="relative">
           <ShoppingCart className="w-6 h-6" />
-
         </div>
 
-        {/* Texto: valor total */}
+        {/* Texto */}
         <span className="flex-1 text-center font-semibold text-base">
           Ver carrinho
         </span>
